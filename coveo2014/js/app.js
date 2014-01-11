5a83e9a6058aebca4d2db743276d85039147a25d
Sylvain Perron <sylvain.perron@protorisk.com>
2014-01-11 15:11:58 -0500

    sHOWING ART

--- coveo2014/js/app.js
+++ coveo2014/js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     $(".facet-slider").slider();
 
@@ -7,4 +7,38 @@ $(document).ready(function() {
             return 'Current value: ' + value;
         }
     });
+
+    updateArtworkFromItunes();
 });
+
+
+function updateArtworkFromItunes() {
+    $("div[data-artist]").each(function(value, key) {
+        console.log(value, key);
+
+        var artist = key.attributes['data-artist'].value;
+        var album = key.attributes['data-album'].value;
+
+        getAlbumArtwork(album, artist).done(function (resp) {
+            console.log(resp);
+            if (resp.results.length > 0)
+                key.innerHTML = '<img class="albumArt" src="' + resp.results[0].artworkUrl100 + '"/>';
+            else {
+                key.innerHTML = '<img class="albumArt" src="http://www.progx.org/users/Gfx/empty_case.png"/>';
+            }
+        });
+    });
+
+}
+
+function getAlbumArtwork(albumName, artistName) {
+    var parsedAlbumName = albumName.split(' ').join('+');
+    var parsedArtistName = artistName.split(' ').join('+');
+
+    return $.ajax({
+        type: "GET",
+        url: "https://itunes.apple.com/search?term=" + parsedAlbumName + "+" + parsedArtistName,
+        dataType: "json"
+    });
+
+}
